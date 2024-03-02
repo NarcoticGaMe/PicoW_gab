60595039845f4743fd5f716467c59d69816369ae
Gabriele Mero <gabrielemero91@gmial.com>
2024-03-02 20:08:10 +0100

    modified:   main.py 	deleted:    update.py

--- main.py
+++ main.py
@@ -2,8 +2,17 @@ from Fixed import SSID, PASSWORD, TOKEN, TELEGRAMID, GITURL
 from picodate import update
 import utelegram
 
+from picozero import LED
+from time import sleep
+
 ## Check for updates
-update(GITURL, SSID, PASSWORD)
+# update(GITURL, SSID, PASSWORD)
+
+
+##
+Green_led = LED(11)
+Yellow_led = LED(12)
+Red_led = LED(13)
 
 #Initialization
 utelegram_config = {
@@ -12,3 +21,19 @@ utelegram_config = {
 print('Staritng Telegram Bot')
 bot = utelegram.ubot(utelegram_config['token'])
 bot.send(TELEGRAMID, 'Bot Online')
+
+def get_message(message):
+    bot.send(message['message']['chat']['id'], message['message']['text'].upper())
+
+def reply_red(message):
+    Red_led.toggle()
+def reply_green(message):
+    Green_led.toggle()
+def reply_yellow(message):
+    Yellow_led.toggle()
+
+bot.register('/red', reply_red)
+bot.register('/yellow', reply_yellow)
+bot.register('/green', reply_green)
+bot.set_default_handler(get_message)
+bot.listen()
--- update.py
+++
@@ -1,7 +0,0 @@
-from ota import OTAUpdater
-from Fixed import SSID, PASSWORD, URL
-
-def update():
-    firmware_url = URL
-    ota_updater = OTAUpdater(SSID, PASSWORD, firmware_url, "main.py")
-    ota_updater.download_and_install_update_if_available()
