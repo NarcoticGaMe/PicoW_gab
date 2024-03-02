847b6bea9cc833f6dfb9d10117e67e30b38c3f69
Gabriele Mero <gabrielemero91@gmial.com>
2024-03-02 17:29:05 +0100

    modified:   main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from ota import OTAUpdater
 from Fixed import SSID, PASSWORD
 
-firmware_url = "https://raw.githubusercontent.com/NarcoticGaMe/PicoW_gab/main"
-ota_updater = OTAUpdater(SSID, PASSWORD, firmware_url, "test.py")
+firmware_url = "https://raw.githubusercontent.com/NarcoticGaMe/PicoW_gab/"
+ota_updater = OTAUpdater(SSID, PASSWORD, firmware_url, "main.py")
 ota_updater.download_and_install_update_if_available()
 
