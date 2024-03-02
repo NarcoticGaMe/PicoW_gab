d8358df617b0225e409b92021ef08d001dbb29b6
Gabriele Mero <gabrielemero91@gmial.com>
2024-03-02 18:25:41 +0100

    modified:   .gitignore 	modified:   Fixed.py 	modified:   main.py 	new file:   update.py

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
-from ota import OTAUpdater
 from Fixed import SSID, PASSWORD, TOKEN, TELEGRAMID
+from update import update
 import utelegram
 
-debug = True
-
 ## Check for updates
-firmware_url = "https://raw.githubusercontent.com/NarcoticGaMe/PicoW_gab/main/"
-ota_updater = OTAUpdater(SSID, PASSWORD, firmware_url, "main.py")
-ota_updater.download_and_install_update_if_available()
+update()
 
 #Initialization
 utelegram_config = {
