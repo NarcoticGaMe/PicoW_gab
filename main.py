7afee9d81ec4db2197b2451102fd4b87e16e23d9
NarcoticGaMe <gabrielemero91@gmial.com>
2024-05-03 23:33:50 +0200

    new file:   SSD1306.py     modified:   main.py

--- main.py
+++ main.py
@@ -9,11 +9,6 @@ from time import sleep
 update(GITURL, SSID, PASSWORD)
 
 
-##
-Green_led = LED(11)
-Yellow_led = LED(12)
-Red_led = LED(13)
-
 #Initialization
 utelegram_config = {
     'token': TOKEN
@@ -24,16 +19,3 @@ bot.send(TELEGRAMID, 'Bot Online')
 
 def get_message(message):
     bot.send(message['message']['chat']['id'], message['message']['text'].upper())
-
-def reply_red(message):
-    Red_led.toggle()
-def reply_green(message):
-    Green_led.toggle()
-def reply_yellow(message):
-    Yellow_led.toggle()
-
-bot.register('/red', reply_red)
-bot.register('/yellow', reply_yellow)
-bot.register('/green', reply_green)
-bot.set_default_handler(get_message)
-bot.listen()
