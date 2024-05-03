26f7b603c1c3900a461416c0f593a219adc7c438
NarcoticGaMe <gabrielemero91@gmial.com>
2024-05-03 23:56:00 +0200

    modified:   main.py

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 from Fixed import SSID, PASSWORD, TOKEN, TELEGRAMID, GITURL
 from picodate import update
 import utelegram
-
+from machine import Pin, I2C
+from ssd1306 import SSD1306_I2C
 from picozero import LED
 from time import sleep
 
 ## Check for updates
 update(GITURL, SSID, PASSWORD)
 
+
+##Display
+WIDTH =128 
+HEIGHT= 64
+i2c=I2C(0,scl=Pin(1),sda=Pin(0),freq=200000)
+oled = SSD1306_I2C(WIDTH,HEIGHT,i2c)
+oled.fill(0)
+oled.text("DIY PROJECTS LAB", 0, 0)
+oled.text("Tutorial", 0, 40)
+oled.show()
 
 #Initialization
 utelegram_config = {
@@ -19,3 +30,5 @@ bot.send(TELEGRAMID, 'Bot Online')
 
 def get_message(message):
     bot.send(message['message']['chat']['id'], message['message']['text'].upper())
+
+bot.set_default_handler(get_message)
